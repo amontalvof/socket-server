5288d70c691fac0794595c1fdd20143c870fe5b3
amontalvof <amontalvof92@gmail.com>
2021-06-06 12:26:44 -0500

    feat: FrontEnd - MiniChat

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const colors = require('colors/safe');
+const express = require('express');
+
 // Express Server
-const app = require('express')();
+const app = express();
+
 // Socket Server
 const server = require('http').createServer(app);
 // Socket Server Configuration
 const io = require('socket.io')(server);
 
+// Display the public directory
+app.use(express.static(__dirname + '/public'));
+
 io.on('connection', () => {
-    /* … */
+    console.log('Client connected!');
 });
 
 server.listen(8080, () => {
